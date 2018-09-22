27cce253431ac8459cd9cb2464ad26b0fbd84e7b
Gabriel <gdeperes@gmail.com>
2018-09-22 04:41:12 -0300

    Improved documentation on Display

--- include/Hardware/Display.hpp
+++ include/Hardware/Display.hpp
@@ -40,6 +40,7 @@ public:
      * @brief Describes the GBA Display VideoModes. 
      * @details The tiled modes support multiple backgrounds.
      * Bitmaps mode only support background 2. Do not forget to enable it.
+     * In tiled modes Charblocks 0 to 4 of 
      */
     enum class VideoMode
     {
@@ -50,21 +51,63 @@ public:
         BitmapPagedPaletted,    /**< Mode 4: Bitmap mode, VRAM is mapped as two pages, each 240x160 8bpp paletted (256 colors) images. */
         BitmapPagedFullColor    /**< Mode 5: Bitmap mode, VRAM is mapped as two pages, each 160x128 16bpp images */
     };
+    /**
+     * @brief Describes the GBA Object Mapping Modes.
+     * @details These affect how object tiles are expected to be arranged in memory.
+     */
     enum class ObjectMappingMode
     {
-        twoDimensional,
-        oneDimensional
+        twoDimensional,        /**<Object VRAM is interpreted as 256px by 256px spritesheet. So the rows of a sprite tiles are offset by the size
+                                of the spritesheet. It is usually better to use the oneDimensional mode. */
+        oneDimensional         /**<Each sprite is interpreted as a separated image. So the second row of as sprite is expected to be right next to the
+                                first row. This mode is usually preffered over twoDimensional mode. */
     };
 private:
     volatile static unsigned short * const displayControlRegister;
 public:
+    /**
+     * @brief Resets the display control register to default (zero);
+     * @details Always use this when the previous state is unknown.
+     */
     static void Clear();
+    /**
+     * @brief Sets the current video mode, changes how VRAM is interpreted.
+     * @see Display::Videomode
+     * @param videoMode Video mode to set display to. Defaults to Mode 0, Tiled with 4 regular backgrounds.
+     */
     static void SetVideoMode(VideoMode videoMode);
+    /**
+     * @brief Selects between the front and back pages for paged bitmaps modes (4 and 5).
+     * @details Swapping pages is used to have the game draw to a buffer, the back page, and only when drawing is done,
+     * display it by swapping pages. Has no effect on non paged modes.
+     * @param pageID Page ID can be either 0 or 1. Defaults to zero.
+     */
     static void SetCurrentPage(int pageId);
+    /**
+     * @brief Though OAM is normally write locked during VDRAW, this enables OAM access during HBlanks.
+     * @details Doing this will reduce how many sprite pixels can be drawn per line.
+     * @param enable True enables access, false disables it. Defaults to false.
+     */
     static void EnableHBlankOAMAccess(bool enable);
+    /**
+     * @brief Sets the current object mapping mode, changes how Object VRAM is interpreted.
+     * @see Display::ObjectMappingMode
+     * @param mode Mapping mode to set display to. Defaults to twoDimensional.
+     */
     static void SetObjectMappingMode(ObjectMappingMode mode);
     static void EnableForcedBlank(bool enable);
+    /**
+     * @brief Enables drawing of a background.
+     * @see Backgroudns
+     * @param backgroundID Which background to enable/disable, number from 0 to 3.
+     * @param enable True is enabled, false is disabled. Defaults to false.
+     */
     static void EnableBackground(int backgroundID, bool enable);
+    /**
+     * @brief Enables drawing of objects.
+     * @see Object
+     * @param enable True is enabled, false is disabled. Defaults to false.
+     */
     static void EnableObjects(bool enable);
     static void EnableBackgroundWindow(int windowID, bool enable);
     static void EnableObjectWindow(bool enable);
