7f605ecf62dd76f92b9df29041ea358c3ef5dcc7
Gabriel <gdeperes@gmail.com>
2018-10-05 21:18:02 -0300

    Adven is now under the MPL-2.0 license

--- include/adven/GameInit.hpp
+++ include/adven/GameInit.hpp
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2018 Gabriel de Moura Peres
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 #ifndef DM_ADVEN_GAMEINIT_HPP
 #define DM_ADVEN_GAMEINIT_HPP
 
--- include/adven/Sprite.hpp
+++ include/adven/Sprite.hpp
@@ -1,6 +1,12 @@
+/*
+ * Copyright 2018 Gabriel de Moura Peres
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 #ifndef DM_ADVEN_SPRITE_HPP
 #define DM_ADVEN_SPRITE_HPP
-
 
 #include <vector>
 #include <advenCore/Types.hpp>
