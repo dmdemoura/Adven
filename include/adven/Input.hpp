ba8b176dcb4ff20d055837bdfd691a81a1db74fc
Amanda de Moura <5531103+dmdemoura@users.noreply.github.com>
2019-06-24 18:15:50 -0300

    Fixed my name

--- include/adven/Input.hpp
+++ include/adven/Input.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Gabriel de Moura Peres
+ * Copyright 2018 Amanda de Moura Peres
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
